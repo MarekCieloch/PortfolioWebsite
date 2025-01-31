4f4dd24c0b992e61c5174b90f5a27f6340ce0d86
RikuEstia <mdcieloch@gmail.com>
2025-01-31 16:38:56 +0100

    Added boredAPI

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,13 +1,55 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
 
 console.log("apiRouter.js loaded");
 
-// READ: Route to get all employees and render the employee list
+// Middleware to serve static files
+router.use(express.static("public"));
+
+// Route to render the fetchapi project page
 router.get("/projects/fetchapi", (req, res) => {
   res.render("projects/fetchapi", {
     title: "Marek Cieloch - Software Engineer",
   });
 });
+
+// Route to handle both GET and POST requests for the boredAPI project
+router
+  .route("/projects/boredAPI")
+  .get(async (req, res) => {
+    try {
+      const response = await axios.get(
+        "https://bored.api.lewagon.com/api/activity/"
+      );
+      const result = response.data;
+      res.render("projects/boredAPI.ejs", { data: result });
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("projects/boredAPI.ejs", { error: error.message });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const { type, participants } = req.body;
+      const apiUrl = `https://bored.api.lewagon.com/api/activity?type=${type}&participants=${participants}`;
+      const response = await axios.get(apiUrl);
+      const result = response.data;
+
+      if (result.error) {
+        res.render("projects/boredAPI.ejs", {
+          error: "No activities match your criteria.",
+        });
+      } else {
+        res.render("projects/boredAPI.ejs", { data: result });
+      }
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("projects/boredAPI.ejs", {
+        error: "An error occurred. Please try again later.",
+      });
+    }
+  });
+
 export default router;
